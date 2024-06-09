db569180f8eaa8527867cc208ad7b476754169d0
Kateryna <fish4715@gmail.com>
2024-06-09 23:54:26 +0300

    + Feat: Implement methods to work with fighters

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -9,5 +9,10 @@ import {
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.get("/api/fighters", (req, res) => {});
+router.get("/api/fighters/:id", (req, res) => {});
+router.post("/api/fighters", (req, res) => {});
+router.patch("/api/fighters/:id", (req, res) => {});
+router.delete("/api/fighters/:id", (req, res) => {});
 
 export { router };
