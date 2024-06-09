d01826ce39f7cdd814f4ac65c53d604924b024df
Kateryna <fish4715@gmail.com>
2024-06-09 20:05:01 +0300

    + Feat: Implement methods to work with user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,5 +9,10 @@ import { responseMiddleware } from "../middlewares/response.middleware.js";
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.get("/api/users", (req, res) => {});
+router.get("/api/users/:id", (req, res) => {});
+router.post("/api/users", (req, res) => {});
+router.patch("/api/users/:id", (req, res) => {});
+router.delete("/api/users/:id", (req, res) => {});
 
 export { router };
