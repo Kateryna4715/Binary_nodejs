63580cf6a6ba24228ea3d870a64803652f73b6ad
Kateryna <fish4715@gmail.com>
2024-06-10 11:38:55 +0300

    + Feat: Implemented validation for user and fighter entities and response middleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,14 @@
 const responseMiddleware = (req, res, next) => {
   // TODO: Implement middleware that returns result of the query
+
+  if (req.status === 200)
+    return res.status(200).json({ error: true, message: "Success" });
+  if (req.status === 400)
+    return res
+      .status(400)
+      .json({ error: true, message: "Problems with validation" });
+  if (req.status === 404)
+    return res.status(404).json({ error: true, message: "Data is not found" });
   next();
 };
 
--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -2,11 +2,92 @@ import { USER } from "../models/user.js";
 
 const createUserValid = (req, res, next) => {
   // TODO: Implement validatior for USER entity during creation
+
+  // Checking for existence
+  const { firstName, lastName, email, phoneNumber, password } = req.body;
+  if (!firstName || !lastName || !email || !phoneNumber || !password)
+    return res
+      .status(404)
+      .json({ error: true, message: "All fields should be filled" });
+
+  // Checking for types
+  if (
+    typeof firstName !== "string" ||
+    typeof lastName !== "string" ||
+    typeof email !== "string" ||
+    typeof phoneNumber !== "string" ||
+    typeof password !== "password"
+  )
+    return res.status(400).json({ error: true, message: "Data is not valid" });
+
+  // Checking for length
+
+  if (password.length < 3)
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is too short" });
+
+  // Checking for gmail
+
+  if (email.slice(-10) !== "@gmail.com")
+    return res.status(400).json({
+      error: true,
+      message: "Your email must belongs to google mail",
+    });
+
+  // Checking for phoneNumber
+
+  if (phoneNumber.slice(0, 5) !== "+380" || phoneNumber.length !== 13)
+    return res.status(400).json({
+      error: true,
+      message: "Your phone number must starts with +380 and have 13 digits",
+    });
   next();
 };
 
 const updateUserValid = (req, res, next) => {
   // TODO: Implement validatior for user entity during update
+
+  // Checking for existence
+  const { firstName, lastName, email, phoneNumber, password } = req.body;
+  if (!firstName || !lastName || !email || !phoneNumber || !password)
+    return res
+      .status(404)
+      .json({ error: true, message: "All fields should be filled" });
+
+  // Checking for types
+  if (
+    typeof firstName !== "string" ||
+    typeof lastName !== "string" ||
+    typeof email !== "string" ||
+    typeof phoneNumber !== "string" ||
+    typeof password !== "password"
+  )
+    return res.status(400).json({ error: true, message: "Data is not valid" });
+
+  // Checking for length
+
+  if (password.length < 3)
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is too short" });
+
+  // Checking for gmail
+
+  if (email.slice(-10) !== "@gmail.com")
+    return res.status(400).json({
+      error: true,
+      message: "Your email must belongs to google mail",
+    });
+
+  // Checking for phoneNumber
+
+  if (phoneNumber.slice(0, 5) !== "+380" || phoneNumber.length !== 13)
+    return res.status(400).json({
+      error: true,
+      message: "Your phone number must starts with +380 and have 13 digits",
+    });
+
   next();
 };
 
